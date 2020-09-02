b452a5229d043436b7a6982c422bbdd9d25dcf27
Serge Vakulenko <serge.vakulenko@gmail.com>
2020-09-02 01:03:12 -0700

    Parse command line arguments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+use std::path;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+pub struct Options {
+    /// Enable trace output
+    #[structopt(short = "t", long = "trace")]
+    trace: bool,
+
+    /// Verbose mode (-v, -vv, -vvv, etc.)
+    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    verbose: u8,
+
+    /// Input file
+    #[structopt(name = "input.oct", parse(from_os_str))]
+    input: path::PathBuf,
+}
+
+pub fn run(options: Options) {
+    println!("{:?}", options);
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use besmlib;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "BESM-6 Simulator")]
+pub struct Cmdline {
+    #[structopt(flatten)]
+    options: besmlib::Options,
+}
+
 pub fn main() {
-    println!("Hello!");
+    let options = Cmdline::from_args().options;
+
+    besmlib::run(options);
 }
